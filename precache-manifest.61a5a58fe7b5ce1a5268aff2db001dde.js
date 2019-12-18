379843780459e63ec36134a420e08f889ab6464a
vasyl <bz.vasok@gmail.com>
2019-12-18 23:14:55 +0200

    Updates

---
+++ precache-manifest.61a5a58fe7b5ce1a5268aff2db001dde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb2844e0aabf6c20a4f6be50ff97733",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "f62967ece99350b7f21b",
+    "url": "/todo/static/css/main.cfa69cb1.chunk.css"
+  },
+  {
+    "revision": "e9f56a2537b5eef3a8f4",
+    "url": "/todo/static/js/2.08f5a4b9.chunk.js"
+  },
+  {
+    "revision": "83b3d04082a8c50f224f3e213e6a5a45",
+    "url": "/todo/static/js/2.08f5a4b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f62967ece99350b7f21b",
+    "url": "/todo/static/js/main.96c0e86c.chunk.js"
+  },
+  {
+    "revision": "9e3d90c4d53deff53c8a",
+    "url": "/todo/static/js/runtime-main.48ad9b76.js"
+  }
+]);
